5c6c1fd3fa1c4ffb01a8d0a29e0006fe78b3d308
Adrian-ortiz0 <dxniel7328@gmail.com>
2026-01-28 09:49:53 -0500

    removí lazy loading

--- src/app/rick-morty/rick-morty.routes.ts
+++ src/app/rick-morty/rick-morty.routes.ts
@@ -1,13 +1,15 @@
 import { Routes } from '@angular/router';
+import { DetailPage } from './pages/detail-page/detail-page';
+import { CharacterListPage } from './pages/character-list-page/character-list-page';
 
 export const rickMortyRoutes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./pages/character-list-page/character-list-page').then(m => m.CharacterListPage),
+    component: CharacterListPage
   },
   {
     path: ':id', 
-    loadComponent: () => import('./pages/detail-page/detail-page').then(m => m.DetailPage),
+    component: DetailPage
   },
 ];
 
